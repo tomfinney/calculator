a1664381d6542f8de190f87393a23f733392f6ef
tomfinney <tomfinney25@gmail.com>
2019-03-31 16:33:15 +0100

    ltos of stylings

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,35 @@ import React from "react";
 import "../../css/normalize.scss";
 import "../../css/core.scss";
 
+import appleSvg from "../../assets/apple.svg";
+
 export default function App() {
   document.title = "Calculator";
-  return <div className="pageWrapper">asasdd</div>;
+  return (
+    <div className="pageWrapper">
+      <div className="pageInner">
+        <header className="header">
+          <img src={appleSvg} alt="the Apple logo" />
+          <span>File</span>
+          <span>Edit</span>
+          <span>View</span>
+          <span>Special</span>
+        </header>
+        <main className="main">
+          <Calculator />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="calculatorBar">Calculator</div>
+      <div className="calculatorInner">
+        <div className="calculatorContent">asdf</div>
+      </div>
+    </div>
+  );
 }
