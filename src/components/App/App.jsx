d8134aa79c2ce7cd288bf1cf2cd59d264015ebf5
tomfinney <tomfinney25@gmail.com>
2019-03-31 17:33:39 +0100

    thingzz

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../../css/normalize.scss";
 import "../../css/core.scss";
@@ -26,11 +26,46 @@ export default function App() {
 }
 
 function Calculator() {
+  const [calculation, setCalculation] = useState(0);
   return (
     <div className="calculator">
       <div className="calculatorBar">Calculator</div>
       <div className="calculatorInner">
-        <div className="calculatorContent">asdf</div>
+        <div className="calculatorContent">
+          <div className="calculatorDisplay">{calculation}</div>
+          <div className="calculatorRow">
+            <div className="calculatorBtn">C</div>
+            <div className="calculatorBtn">E</div>
+            <div className="calculatorBtn">=</div>
+            <div className="calculatorBtn">*</div>
+          </div>
+          <div className="calculatorRow">
+            <div className="calculatorBtn">7</div>
+            <div className="calculatorBtn">8</div>
+            <div className="calculatorBtn">9</div>
+            <div className="calculatorBtn">/</div>
+          </div>
+          <div className="calculatorRow">
+            <div className="calculatorBtn">4</div>
+            <div className="calculatorBtn">5</div>
+            <div className="calculatorBtn">6</div>
+            <div className="calculatorBtn">-</div>
+          </div>
+          <div className="calculatorColumns">
+            <div>
+              <div className="calculatorRow">
+                <div className="calculatorBtn">1</div>
+                <div className="calculatorBtn">2</div>
+                <div className="calculatorBtn">3</div>
+              </div>
+              <div className="calculatorRow calculatorRow--last">
+                <div className="calculatorBtn calculatorBtn--wide">0</div>
+                <div className="calculatorBtn calculatorBtn--bottom">.</div>
+              </div>
+            </div>
+            <div className="calculatorBtn calculatorBtn--tall">+</div>
+          </div>
+        </div>
       </div>
     </div>
   );
