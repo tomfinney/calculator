c56fd0435c241bab9eae1ac9bdb5e2b15d0b93c9
tom@ <tom.finney25@googlemail.com>
2019-03-31 15:37:26 +0100

    start of the great calc app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../../css/normalize.scss";
+import "../../css/core.scss";
+
+export default function App() {
+  document.title = "Calculator";
+  return <div className="pageWrapper">asd</div>;
+}
